88a5d4258885a974c4018923a2f3052587886a9d
motypes <mohammed.jalil@gmail.com>
2023-11-08 17:05:35 +0800

    Fixed issues with ffmpeg

---
+++ ISSUES.md
@@ -0,0 +1,35 @@
+
+
+### FFMPEG Issues 
+
+If you run into issues with fluent-ffmpeg you'll need to:
+1. make sure to install ffmpeg on your machine so that the binary is available
+2. update nextjs config to avoid the error below:
+
+```
+Import trace for requested module:
+./lib/videohelper.ts
+./app/api/video/upload/route.ts
+ ⨯ ./node_modules/fluent-ffmpeg/index.js:1:48
+Module not found: Can't resolve './lib-cov/fluent-ffmpeg'
+```
+**Update nextjs config**
+
+```
+const path = require('path');
+
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.resolve.alias['fluent-ffmpeg$'] = path.resolve(
+        __dirname,
+        'node_modules/fluent-ffmpeg/lib/fluent-ffmpeg.js'
+      );
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
+
+```
--- app/api/video/upload/route.ts
+++ app/api/video/upload/route.ts
@@ -1,13 +1,40 @@
 
 import { NextRequest , NextResponse } from "next/server";
+import { writeFile } from "fs/promises";
+import { extractAudioFromVideo } from "@/lib/videohelper";
 
 export async function POST(req: NextRequest, res: NextResponse) {
   console.log("uploading video");
   console.log(req.body);
 
-//    Return a 200 response
+  const data = await req.formData();
+  const file : File = data.get("file") as unknown as File;
 
-    return new NextResponse("OK", {
+  if(!file) {
+    return new NextResponse("No file found", {
+      status: 400,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    })
+  }
+
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+
+  const path = `/tmp/${file.name}`;
+  await writeFile(path, buffer);
+
+  console.log("file written to", path);
+
+//   extract audio from video
+
+  console.log("extracting audio")
+  await extractAudioFromVideo(path, "/tmp/audio.mp3")
+
+   //    Return a 200 response
+
+    return new NextResponse("File uploaded",{
         status: 200,
         headers: {
             "Content-Type": "text/plain",
