2d5d3d456acea8f6bea65f9db166cd190c4f0f20
motypes <mohammed.jalil@gmail.com>
2023-11-09 22:24:26 +0800

    added light theme

--- components/Assistant.tsx
+++ components/Assistant.tsx
@@ -1,4 +1,46 @@
+'use client';
+import { ChangeEvent, FormEvent, useState } from "react";
+
 const Assistant = () => {
+    const [hasInput, setHasInput] = useState(false);
+    const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setHasInput(true);
+    }
+    const handleClear = (e : FormEvent<HTMLFormElement> ) => {
+        e.preventDefault();
+        setHasInput(false);
+    }
+
+    const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setHasInput(true);
+    }
+
+  // Render the search/upload UI if there's no user input yet
+  if (!hasInput) {
+    return (
+      <div className="assistant bg-white rounded shadow p-4 flex flex-col">
+        <h2 className="text-lg font-bold mb-4">Where would you like to go?</h2>
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            placeholder="Start typing..."
+            className="mb-4 p-2 border border-gray-300 rounded"
+          />
+          <input
+            type="file"
+            onChange={handleFileUpload}
+            className="mb-4 p-2 border border-gray-300 rounded"
+          />
+          <button type="submit" className="p-2 bg-blue-500 text-white rounded">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+
   return (
     <div className="assistant">
       <p>Welcome to Wanderlust. Start planning your next journey!</p>
